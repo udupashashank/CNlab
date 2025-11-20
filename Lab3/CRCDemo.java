4fab23aef6ed8eaa2d2eb3c754a4ac0bf0dd1a8c
udupashashank <shashankudupa977@gmail.com>
2025-11-20 15:33:28 +0530

    Create CRCDemo.java

---
+++ Lab3/CRCDemo.java
@@ -0,0 +1,88 @@
+package Lab3;
+import java.util.Scanner; 
+public class CRCDemo { 
+ static String msg; 
+    static String genPoly = "10001000000100001"; 
+    // static String genPoly = "1101"; 
+ 
+    static char t[] = new char[128]; /* Array for message appended with checksum */ 
+    static char cs[] = new char[128]; /* Temporary array for checksum calculation */ 
+    static char g[] = new char[128];  /* Array to store generator polynomial */ 
+ 
+    static int mlen, glen, x, c, flag = 0, test; 
+ 
+    public static void main(String[] args) { 
+        Scanner in = new Scanner(System.in); 
+ 
+        System.out.println("Enter the message to be transferred"); 
+        msg = in.nextLine(); 
+        mlen = msg.length(); /* Length of the original message */ 
+ 
+        for (int i = 0; i < mlen; i++) 
+            t[i] = msg.charAt(i); 
+ 
+        System.out.println("Predefined Generator Polynomial is: " + genPoly); 
+        g = genPoly.toCharArray(); 
+        glen = genPoly.length(); /* Length of Generator Polynomial */ 
+ 
+        for (x = mlen; x < (mlen + glen - 1); x++) 
+            t[x] = '0'; 
+ 
+        System.out.println("Zero extended message is: " + new String(t)); 
+ 
+        crc(); /* Checksum computation */ 
+ 
+        System.out.println("CheckSum is: " + new String(cs)); 
+ 
+        /* Subtract the checksum from zero extended message which means we do 
+simple "XORing" */ 
+        for (x = mlen; x < mlen + glen - 1; x++) 
+            t[x] = cs[x - mlen]; 
+ 
+        System.out.println("Final codeword generated is: " + new String(t)); 
+ 
+        System.out.println("\n\nTest Error detection 1(yes) 0(no) ? : "); 
+        test = in.nextInt(); 
+ 
+        if (test == 1) { 
+            System.out.println("Enter position where error is to inserted: "); 
+            x = in.nextInt(); 
+            t[x] = (t[x] == '0') ? '1' : '0'; 
+            System.out.println("Erroneous data: " + new String(t)); 
+        } 
+ 
+        crc(); // computation at receiver side 
+ 
+        for (x = 0; x < (glen - 1); x++) { 
+            if (cs[x] == '1') { 
+                flag = 1; 
+                break; 
+            } 
+        } 
+ 
+        if (flag == 1) 
+            System.out.println("Error was detected during transfer"); 
+        else 
+            System.out.println("No Error Detected during transfer"); 
+    } 
+ 
+    public static void crc() { 
+        for (x = 0; x < glen; x++) 
+            cs[x] = t[x]; 
+ 
+        do { 
+            if (cs[0] == '1') 
+                xor(); 
+ 
+            for (c = 0; c < glen - 1; c++) { 
+                cs[c] = cs[c + 1]; 
+            } 
+            cs[c] = t[x++]; 
+        } while (x <= mlen + glen - 1); 
+    } 
+ 
+    public static void xor() { 
+        for (c = 1; c < glen; c++) 
+            cs[c] = ((cs[c] == g[c]) ? '0' : '1'); 
+    } 
+}
